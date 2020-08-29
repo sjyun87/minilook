6168415e01755a385c5b2b9e2747a2b6aa2e0e8a
윤성준 <saira_@naver.com>
2020-08-29 09:49:29 +0900

    [네이버 로그인] 파일 추가

--- app/src/main/java/com/minilook/minilook/ui/login/naver/NaverLoginManager.java
+++ app/src/main/java/com/minilook/minilook/ui/login/naver/NaverLoginManager.java
@@ -2,48 +2,46 @@ package com.minilook.minilook.ui.login.naver;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-//import android.content.Context;
-//import androidx.core.content.ContextCompat;
-//import com.minilook.minilook.R;
-//import com.nhn.android.naverlogin.OAuthLogin;
-//import com.nhn.android.naverlogin.OAuthLoginHandler;
-//import timber.log.Timber;
+import com.minilook.minilook.R;
+import com.nhn.android.naverlogin.OAuthLogin;
+import com.nhn.android.naverlogin.OAuthLoginHandler;
+import timber.log.Timber;
 
 public class NaverLoginManager {
 
-    //private Activity activity;
-    //private OAuthLogin oAuthLogin;
+    private Activity activity;
+    private OAuthLogin oAuthLogin;
 
     public NaverLoginManager(Activity activity) {
-        //this.activity = activity;
-        //this.oAuthLogin = OAuthLogin.getInstance();
-        //init();
+        this.activity = activity;
+        this.oAuthLogin = OAuthLogin.getInstance();
+        init();
     }
 
     private void init() {
-        //oAuthLogin.init(
-        //    activity,
-        //    activity.getString(R.string.naver_client_id),
-        //    activity.getString(R.string.naver_client_secret),
-        //    activity.getString(R.string.naver_client_app_name)
-        //);
+        oAuthLogin.init(
+            activity,
+            activity.getString(R.string.naver_client_id),
+            activity.getString(R.string.naver_client_secret),
+            activity.getString(R.string.naver_client_app_name)
+        );
     }
 
     @SuppressLint("HandlerLeak")
     public void login() {
-        //oAuthLogin.startOauthLoginActivity(activity, new OAuthLoginHandler() {
-        //    @Override public void run(boolean success) {
-        //        if (success) {
-        //            Timber.e("Naver Login :: Access Token :: %s", oAuthLogin.getAccessToken(activity));
-        //        } else {
-        //            Timber.e("Naver Login :: error code = %s / error message = %s",
-        //                oAuthLogin.getLastErrorCode(activity).getCode(), oAuthLogin.getLastErrorDesc(activity));
-        //        }
-        //    }
-        //});
+        oAuthLogin.startOauthLoginActivity(activity, new OAuthLoginHandler() {
+            @Override public void run(boolean success) {
+                if (success) {
+                    Timber.e("Naver Login :: Access Token :: %s", oAuthLogin.getAccessToken(activity));
+                } else {
+                    Timber.e("Naver Login :: error code = %s / error message = %s",
+                        oAuthLogin.getLastErrorCode(activity).getCode(), oAuthLogin.getLastErrorDesc(activity));
+                }
+            }
+        });
     }
 
     public void logout() {
-        //oAuthLogin.logout(activity);
+        oAuthLogin.logout(activity);
     }
 }
