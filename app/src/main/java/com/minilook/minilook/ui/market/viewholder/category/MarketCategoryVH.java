2102c533ac6fbea3e4df25128f02e47de93511c1
윤성준 <saira_@naver.com>
2020-10-29 16:55:05 +0900

    [마켓] 카테고리 모듈 UI 작업

---
+++ app/src/main/java/com/minilook/minilook/ui/market/viewholder/category/MarketCategoryVH.java
@@ -0,0 +1,65 @@
+package com.minilook.minilook.ui.market.viewholder.category;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.core.view.ViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+import butterknife.BindDimen;
+import butterknife.BindView;
+import com.fondesa.recyclerviewdivider.DividerDecoration;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.minilook.minilook.R;
+import com.minilook.minilook.data.model.commercial.CommercialDataModel;
+import com.minilook.minilook.data.model.common.CodeDataModel;
+import com.minilook.minilook.data.model.market.MarketDataModel;
+import com.minilook.minilook.ui.base.BaseViewHolder;
+import com.minilook.minilook.ui.market.viewholder.category.adapter.MarketCategoryAdapter;
+import com.minilook.minilook.ui.market.viewholder.commercial.adapter.MarketCommercialAdapter;
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketCategoryVH extends BaseViewHolder<MarketDataModel> {
+
+    @BindView(R.id.rcv_category) RecyclerView recyclerView;
+
+    @BindDimen(R.dimen.dp_5) int dp_5;
+
+    private MarketCategoryAdapter adapter;
+    private Gson gson = new Gson();
+
+    public MarketCategoryVH(@NonNull View itemView) {
+        super(LayoutInflater.from(itemView.getContext())
+            .inflate(R.layout.item_market_category, (ViewGroup) itemView, false));
+        setupRecyclerView();
+    }
+
+    private void setupRecyclerView() {
+        adapter = new MarketCategoryAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(context, RecyclerView.HORIZONTAL, false));
+        recyclerView.setAdapter(adapter);
+        DividerDecoration.builder(context)
+            .size(dp_5)
+            .asSpace()
+            .build()
+            .addTo(recyclerView);
+        ViewCompat.setNestedScrollingEnabled(recyclerView, false);
+    }
+
+    @Override public void bind(MarketDataModel $data) {
+        super.bind($data);
+
+        adapter.set(parseJsonToModel());
+        adapter.refresh();
+    }
+
+    private List<CommercialDataModel> parseJsonToModel() {
+        return gson.fromJson(data.getData(), new TypeToken<ArrayList<CodeDataModel>>() {
+        }.getType());
+    }
+}
---
+++ app/src/main/java/com/minilook/minilook/ui/market/viewholder/category/viewholder/MarketCategoryItemVH.java
@@ -0,0 +1,53 @@
+package com.minilook.minilook.ui.market.viewholder.category.viewholder;
+
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import butterknife.BindDrawable;
+import butterknife.BindView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.minilook.minilook.R;
+import com.minilook.minilook.data.code.CommercialType;
+import com.minilook.minilook.data.model.commercial.CommercialDataModel;
+import com.minilook.minilook.data.model.common.CodeDataModel;
+import com.minilook.minilook.ui.base.BaseViewHolder;
+import com.minilook.minilook.ui.event_detail.EventDetailActivity;
+import com.minilook.minilook.ui.product_detail.ProductDetailActivity;
+import com.minilook.minilook.ui.promotion_detail.PromotionDetailActivity;
+
+public class MarketCategoryItemVH extends BaseViewHolder<CodeDataModel> {
+
+    @BindView(R.id.img_icon) ImageView iconImageView;
+    @BindView(R.id.txt_name) TextView nameTextView;
+
+    @BindDrawable(R.drawable.placeholder_image) Drawable img_placeholder;
+
+    public MarketCategoryItemVH(@NonNull View itemView) {
+        super(LayoutInflater.from(itemView.getContext())
+            .inflate(R.layout.item_market_category_item, (ViewGroup) itemView, false));
+    }
+
+    @Override public void bind(CodeDataModel $data) {
+        super.bind($data);
+
+        Glide.with(context)
+            .load(data.getIconUrl())
+            .placeholder(img_placeholder)
+            .error(img_placeholder)
+            .transition(new DrawableTransitionOptions().crossFade())
+            .into(iconImageView);
+
+        nameTextView.setText(data.getName());
+
+        itemView.setOnClickListener(this::onItemClick);
+    }
+
+    void onItemClick(View view) {
+
+    }
+}
